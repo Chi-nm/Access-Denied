7791a42e268a6a367092e256f99e9c165c071d51
Chi-nm <chinmayee.guruprasad@gmail.com>
2024-05-18 11:39:15 +0530

    ini

---
+++ component/Main/page.tsx
@@ -0,0 +1,15 @@
+export default function Main(){
+    return<>
+        return(
+        <div className="main">
+            <div className="nav">
+                <p>Gemini</p>
+                <img src={assets.user_icon} alt=""/>
+            </div>
+            <div className="main-container">
+                
+            </div>
+        </div>
+    )
+    </>
+}
